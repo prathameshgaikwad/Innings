35f9d29a8b06800e82529916f07b7a064063c22e
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-03-10 23:04:13 +0530

    removed tournament fixtures route from fixtures and setup in tournament routes

--- server/routes/fixtures.js
+++ server/routes/fixtures.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const {
-  getAllFixtures,
   getFixtureDetails,
   createFixturesBatch,
 } = require("../controllers/fixtures");
 
 const router = express.Router();
 
-router.get("/:tournamentId/all", getAllFixtures);
 router.get("/:fixtureId", getFixtureDetails);
 router.post("/create-batch", createFixturesBatch);
 
