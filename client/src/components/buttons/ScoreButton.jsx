87359cb3c8a1fab3c561ec7aa20e88e996ee925a
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-03-16 22:49:14 +0530

    sent matchId

--- client/src/components/buttons/ScoreButton.jsx
+++ client/src/components/buttons/ScoreButton.jsx
@@ -3,22 +3,25 @@
 import { Button, Typography } from "@mui/joy";
 
 import { GoDotFill } from "react-icons/go";
+import { useParams } from "react-router-dom";
 
 const ScoreButton = ({ score, type, socket }) => {
+  const { matchId } = useParams();
+
   const handleClick = () => {
     if (type === "extra") {
       const extraLogItem = {
         extraType: score,
         runs: 1,
       };
-      socket.emit("addExtra", extraLogItem);
+      socket.emit("addExtra", extraLogItem, matchId);
     } else {
       const runLogItem = {
         playerId: 1,
         score: type === "dot" ? 0 : parseInt(score),
         bowlerId: 2,
       };
-      socket.emit("addRun", runLogItem);
+      socket.emit("addRun", runLogItem, matchId);
     }
   };
   return (
