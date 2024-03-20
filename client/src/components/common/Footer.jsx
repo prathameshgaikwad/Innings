86c4620ecb61b564fb9a30ea5ff98c32efa8bdc3
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-03-20 19:29:52 +0530

    added link to footer

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography, useTheme } from "@mui/joy";
+import { Box, Divider, Link, Typography, useTheme } from "@mui/joy";
 
 import CopyrightIcon from "@mui/icons-material/Copyright";
 
@@ -6,27 +6,41 @@ const Footer = () => {
   const theme = useTheme();
   return (
     <>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 1,
-          p: 4,
-          borderTop: "1px solid",
-          borderColor: "divider",
-        }}>
-        <Typography
-          level="body-sm"
-          sx={{ display: "inline-flex", color: theme.palette.text.secondary }}
-          startDecorator={<CopyrightIcon />}>
-          {new Date().getFullYear()} Innings
-        </Typography>
-        |
-        <Typography level="body-sm" sx={{ color: theme.palette.text.tertiary }}>
-          The Ultimate Cricket Tournament Platform
-        </Typography>
+      <Box>
+        <Link
+          href="/"
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: 1,
+            p: 4,
+            borderTop: "1px solid",
+            borderColor: "divider",
+            "&:hover": {
+              textDecoration: "none",
+            },
+          }}>
+          <Typography
+            level="body-sm"
+            sx={{ display: "inline-flex", color: theme.palette.text.secondary }}
+            startDecorator={<CopyrightIcon />}>
+            {new Date().getFullYear()} Innings
+          </Typography>
+          <Divider
+            orientation="vertical"
+            sx={{
+              "--Divider-thickness": "1px",
+              color: theme.palette.text.tertiary,
+            }}
+          />
+          <Typography
+            level="body-sm"
+            sx={{ color: theme.palette.text.tertiary }}>
+            The Ultimate Cricket Tournament Platform
+          </Typography>
+        </Link>
       </Box>
     </>
   );
