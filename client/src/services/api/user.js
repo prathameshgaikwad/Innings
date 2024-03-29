cc06eb47a5607734bf3bf8aa83802fd9ba4e1272
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-03-29 12:15:04 +0530

    refactor: moved remaining api calls to user api file

--- client/src/services/api/user.js
+++ client/src/services/api/user.js
@@ -1,10 +1,13 @@
 import {
   setLiveMatch,
+  setLogin,
   setNoLiveMatch,
   setUpcomingMatches,
 } from "../../state/user/userSlice";
 
 const TOURNAMENTS_API = import.meta.env.VITE_SERVER_TOURNAMENTS_API;
+const REGISTER_ENDPOINT = import.meta.env.VITE_SERVER_REGISTER_URL;
+const LOGIN_ENDPOINT = import.meta.env.VITE_SERVER_LOGIN_URL;
 
 export const getLiveMatchInfo =
   ({ tournamentId, token, setIsLoading }) =>
@@ -59,3 +62,119 @@ export const getUpcomingMatches =
       console.log("error:", error);
     }
   };
+
+export const signUpGoogle = async (access_token, navigate) => {
+  try {
+    const response = await fetch(REGISTER_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ access_token }),
+    });
+    if (response.ok) {
+      navigate("/");
+    } else {
+      const errorData = await response.json();
+      console.error("Signup failed:", errorData.message);
+    }
+  } catch (error) {
+    console.log("Error during signup: ", error);
+  }
+};
+
+export const signInGoogle = async (access_token, navigate, dispatch) => {
+  try {
+    const response = await fetch(LOGIN_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ access_token }),
+    });
+
+    if (response.ok) {
+      const responseData = await response.json();
+      dispatch(
+        setLogin({
+          user: responseData.user,
+          token: responseData.token,
+        })
+      );
+      navigate("/");
+    } else {
+      const errorData = await response.json();
+      console.error("SignIn failed:", errorData.message);
+    }
+  } catch (error) {
+    console.log("Error during signIn: ", error);
+  }
+};
+
+export const registerUser = async (
+  values,
+  resetForm,
+  setIsFailedResponseVisible,
+  setIsSuccessVisible,
+  navigate
+) => {
+  try {
+    const response = await fetch(REGISTER_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    if (response.ok) {
+      setIsSuccessVisible(true);
+      const responseData = await response.json();
+
+      if (responseData) {
+        setTimeout(() => {
+          resetForm();
+          navigate("/accounts/sign-in");
+        }, 2000);
+      }
+    } else {
+      if (response.status === 400) {
+        setIsFailedResponseVisible(true);
+        setTimeout(() => {
+          setIsFailedResponseVisible(false);
+          resetForm();
+        }, 2400);
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+export const signInUser = async (values, resetForm, dispatch, navigate) => {
+  try {
+    const response = await fetch(LOGIN_ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+
+    if (response.ok) {
+      const responseData = await response.json();
+      resetForm();
+
+      if (responseData) {
+        dispatch(
+          setLogin({
+            user: responseData.user,
+            token: responseData.token,
+          })
+        );
+        navigate("/");
+      }
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
