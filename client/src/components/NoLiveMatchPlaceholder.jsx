ace7acff1e445e79bc23f53f9f2ad6add5890114
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-03-31 18:03:54 +0530

    fix: fixed width value is boolean issue

--- client/src/components/NoLiveMatchPlaceholder.jsx
+++ client/src/components/NoLiveMatchPlaceholder.jsx
@@ -1,20 +1,14 @@
-import { Typography, useTheme } from "@mui/joy";
-
-import { useMediaQuery } from "@mui/material";
+import { Typography } from "@mui/joy";
 
 const NoLiveMatchPlaceholder = () => {
-  const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
-
   return (
     <>
       <img
         src="assets/no_live_match_placeholder.svg"
         alt="No Live Match"
         height={400}
-        width={isMobile && "100%"}
       />
-      <Typography level="body-lg">No Matches are live now!</Typography>
+      <Typography level="body-lg">No Matches are live right now!</Typography>
     </>
   );
 };
