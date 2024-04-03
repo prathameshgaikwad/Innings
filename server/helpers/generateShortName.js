51d02240e62e7b1d2c56ad5aa96486f0e71e6547
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-03 19:25:12 +0530

    chore: added abbreviate package to generate short names

--- server/helpers/generateShortName.js
+++ server/helpers/generateShortName.js
@@ -1,12 +1,18 @@
+const abbreviate = require("abbreviate");
+
 const generateShortName = (name) => {
   const words = name.split(" ");
   let shortName = "";
 
-  words.forEach((word) => {
-    shortName += word.charAt(0).toUpperCase();
-  });
+  if (words.length === 2 || words.length === 3) {
+    words.map((word) => {
+      shortName += word.charAt(0);
+    });
+  } else {
+    shortName = abbreviate(name, { length: 3 });
+  }
 
-  return shortName;
+  return shortName.toUpperCase();
 };
 
 module.exports = {
