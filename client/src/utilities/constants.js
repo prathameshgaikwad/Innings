5376d23f381e2fb029496e4e0bc983d31fcae625
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-05 16:48:44 +0530

    refactor: Separate bg art url constants

---
+++ client/src/utilities/constants.js
@@ -0,0 +1,5 @@
+export const BACKGROUND_ART_LIGHT_URL =
+  "https://images.unsplash.com/photo-1629285483773-6b5cde2171d7?q=80&w=1965&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+export const BACKGROUND_ART_DARK_URL =
+  "https://images.unsplash.com/photo-1587385789097-0197a7fbd179?q=80&w=2060&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
