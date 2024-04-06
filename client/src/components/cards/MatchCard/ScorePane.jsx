b762ddd60c3d7200632d7a39fd15ba96cbf8e412
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-06 23:38:52 +0530

    feat: Opponent score now shown

--- client/src/components/cards/MatchCard/ScorePane.jsx
+++ client/src/components/cards/MatchCard/ScorePane.jsx
@@ -3,7 +3,11 @@
 import { Box, Divider, Stack, Typography, useTheme } from "@mui/joy";
 
 import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord";
+import { VscGitCommit } from "react-icons/vsc";
 import { useMediaQuery } from "@mui/material";
+
+const OPPONENT_SCORE = 196;
+const OPPONENT_WICKETS = 6;
 
 const ScorePane = ({
   totalRuns,
@@ -11,10 +15,12 @@ const ScorePane = ({
   oversCompleted,
   overs,
   currentRunRate,
+  innings = 2,
 }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const isTextOverflow = useMediaQuery(theme.breakpoints.down(1015));
+  const isSecondInnings = innings === 2;
   return (
     <Box
       sx={{
@@ -33,6 +39,21 @@ const ScorePane = ({
         <Typography level={isMobile ? "h3" : "h2"}>
           {totalRuns} - {totalWickets}
         </Typography>
+        {isSecondInnings && (
+          <>
+            <VscGitCommit
+              style={{
+                mx: isMobile ? 0.5 : 1,
+                color: "InactiveBorder",
+              }}
+            />
+            <Typography
+              level={isMobile ? "h4" : "h3"}
+              sx={{ color: "InactiveCaptionText" }}>
+              {OPPONENT_SCORE}/{OPPONENT_WICKETS}
+            </Typography>
+          </>
+        )}
       </Stack>
       <Divider sx={{ my: isMobile ? 0.5 : 1 }} />
       <Box sx={{ display: "flex", alignItems: "center" }}>
