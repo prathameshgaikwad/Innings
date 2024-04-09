5f57c08a82248bb0a361e8c51f92be588c4c4547
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-09 13:05:51 +0530

    feat: Add QueryClientProvider for tanstack/react-query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { persistor, store } from "./store.js";
 
 import App from "./App.jsx";
@@ -10,14 +11,18 @@ import ReactDOM from "react-dom/client";
 const GOOGLE_OAUTH_CLIENT_ID = import.meta.env
   .VITE_SERVER_GOOGLE_OAUTH_CLIENT_ID;
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-        </PersistGate>
-      </Provider>
-    </GoogleOAuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
+        </Provider>
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
