463bc038b2559114ed909d0b2446fcc5af04847a
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-10 21:19:07 +0530

    refactor: removed unnecessary helper

--- server/helpers/setInningsData.js
+++
@@ -1,6 +0,0 @@
-const setInningsData = ({ data, battingTeam, team1_id, team2_id }) => {
-  if (battingTeam._id === team1_id) return data.team1;
-  else return data.team2;
-};
-
-module.exports = { setInningsData };
