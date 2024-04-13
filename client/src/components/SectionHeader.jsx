73385a3d1b3114a750f36aeb15315382a831e223
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-13 15:37:30 +0530

    refactor: Common section header component

---
+++ client/src/components/SectionHeader.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable react/prop-types */
+
+import { Box, Divider, Typography, useTheme } from "@mui/joy";
+
+import { useMediaQuery } from "@mui/material";
+
+const SectionHeader = ({ title, color, startDecorator: StartDecorator }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  return (
+    <Box sx={{ width: "100%", mb: isMobile ? 3 : 6 }}>
+      <Divider>
+        <Typography
+          level={isMobile ? "h4" : "h3"}
+          sx={{ mx: 2 }}
+          color={color}
+          startDecorator={StartDecorator && <StartDecorator />}>
+          {title}
+        </Typography>
+      </Divider>
+    </Box>
+  );
+};
+
+export default SectionHeader;
