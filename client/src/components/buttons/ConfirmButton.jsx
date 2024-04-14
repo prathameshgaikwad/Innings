8da7839c7f773a4b90938c111964046c49c1027d
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-14 17:34:49 +0530

    refactor: remove dispatch events

--- client/src/components/buttons/ConfirmButton.jsx
+++ client/src/components/buttons/ConfirmButton.jsx
@@ -1,21 +1,12 @@
 /* eslint-disable react/prop-types */
 
-import {
-  completeFirstInnings,
-  setBattingTeam,
-} from "../../state/match/matchManagementSlice";
-
 import { Button } from "@mui/joy";
-import { useDispatch } from "react-redux";
 
 const ConfirmButton = ({ setOpen, title, useCase }) => {
   const isSwitch = useCase === "Switch";
-  const dispatch = useDispatch();
 
   const handleSwitch = () => {
-    dispatch(completeFirstInnings());
-    dispatch(setBattingTeam());
-    setOpen(false);
+    alert("Switching sides");
   };
 
   return (
