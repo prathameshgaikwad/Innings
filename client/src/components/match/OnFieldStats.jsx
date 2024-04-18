5a28d3698f7f4bb484299f0c37c9a819adbb0a81
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-18 16:24:26 +0530

    style: enclose inside CenteredBox

--- client/src/components/match/OnFieldStats.jsx
+++ client/src/components/match/OnFieldStats.jsx
@@ -1,18 +1,19 @@
 /* eslint-disable react/prop-types */
 
 import BallLogList from "../lists/BallLogList";
+import CenteredBox from "../layouts/pages/CenteredBox";
 import OnFieldBatsmenStats from "./OnFieldBatsmenStats";
 
 const OnFieldStats = ({ isLoading, batsmenData, ballLog, isSmall = true }) => {
   return (
-    <>
+    <CenteredBox customStyles={{ gap: 1 }}>
       <OnFieldBatsmenStats
         data={batsmenData}
         isSmall={isSmall}
         isLoading={isLoading}
       />
       <BallLogList data={ballLog} isLoading={isLoading} />
-    </>
+    </CenteredBox>
   );
 };
 
