dc58b360b080f3758c9a26c332916c5399972922
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-19 23:54:20 +0530

    refactor: modularize socket code into SocketProvider

---
+++ client/src/components/SocketProvider.jsx
@@ -0,0 +1,66 @@
+/* eslint-disable react/prop-types */
+
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
+import CustomToast from "./notifications/toasts/CustomToast";
+
+const SocketProvider = ({ matchId, socket, isAdmin, children }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const username = `${user.first_name}`;
+
+  const [isSocketConnected, setIsSocketConnected] = useState(false);
+  const [isSocketDisconnected, setIsSocketDisconnected] = useState(false);
+
+  useEffect(() => {
+    if (socket) {
+      socket.on("connect", () => {
+        setIsSocketConnected(true);
+        socket.emit("subscribeToMatch", matchId);
+
+        socket.on("getBallLog", (ball_log) => {
+          console.log("Got some ball log...", ball_log);
+        });
+      });
+
+      socket.on("disconnect", () => {
+        setIsSocketDisconnected(true);
+      });
+    }
+
+    return () => {
+      if (socket) {
+        socket.off("connect");
+        socket.off("getBallLog");
+        socket.off("disconnect");
+      }
+    };
+  }, [socket, matchId, dispatch]);
+
+  return (
+    <>
+      {isSocketConnected && (
+        <CustomToast
+          color={"success"}
+          content={`Hey ${username}, ${
+            isAdmin
+              ? `you're in control of this match!`
+              : `you're watching this match live!`
+          }`}
+          duration={4000}
+        />
+      )}
+      {isSocketDisconnected && (
+        <CustomToast
+          color={"danger"}
+          content={`Connection lost! Please refresh the page and try again.`}
+          duration={4000}
+        />
+      )}
+      {children}
+    </>
+  );
+};
+
+export default SocketProvider;
