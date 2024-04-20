f526bf9f2f132481563967c80a515bf4dae4e4af
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-04-20 16:37:57 +0530

    fix: use latest model convention for property names

--- server/socket/handlers/addPlayerRuns.js
+++ server/socket/handlers/addPlayerRuns.js
@@ -1,15 +1,15 @@
 const Player = require("../../models/player");
 
-const addPlayerRuns = (player_id, runs) => {
+const addPlayerRuns = ({ player_id, runs_scored }) => {
   try {
     const updatePlayerRunLog = async () => {
-      const player = await Player.findById({ _id: player_id });
+      const player = await Player.findById(player_id);
 
-      player.statistics.runs += runs;
+      player.statistics.total_runs += runs_scored;
 
-      if (runs === 4) {
+      if (runs_scored === 4) {
         player.statistics.fours++;
-      } else if (runs === 6) {
+      } else if (runs_scored === 6) {
         player.statistics.sixes++;
       }
       await player.save();
