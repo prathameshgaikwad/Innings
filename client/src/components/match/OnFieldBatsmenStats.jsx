07b717bb150e879a082b94598e03435598912dc9
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-01 21:53:26 +0530

    refactor: simplified layout code

--- client/src/components/match/OnFieldBatsmenStats.jsx
+++ client/src/components/match/OnFieldBatsmenStats.jsx
@@ -1,11 +1,19 @@
 /* eslint-disable react/prop-types */
 
-import { Card, Divider, Grid, Typography, useTheme } from "@mui/joy";
+import {
+  Card,
+  Divider,
+  IconButton,
+  Stack,
+  Typography,
+  useTheme,
+} from "@mui/joy";
 import {
   setOffStrikeBatsman,
   setOnStrikeBatsman,
 } from "../../state/match/matchManagementSlice";
 
+import { HiSelector } from "react-icons/hi";
 import OnFieldBatsmenStatsSkeleton from "../skeletons/OnFieldBatsmenStatsSkeleton";
 import SelectPlayer from "../matchManagement/SelectPlayer";
 import SimpleTextFallback from "../fallbacks/SimpleTextFallback";
@@ -23,6 +31,11 @@ const OnFieldBatsmenStats = ({ isSmall, data, isLoading, isAdmin = false }) => {
 
   const theme = useTheme();
   const isDarkTheme = theme.palette.mode === "dark";
+
+  const handlePlayerChange = () => {
+    alert("Requesting player change");
+  };
+
   return (
     <Card
       variant="outlined"
@@ -31,7 +44,6 @@ const OnFieldBatsmenStats = ({ isSmall, data, isLoading, isAdmin = false }) => {
         display: "inline-flex",
         flexDirection: "row",
         alignItems: "center",
-        justifyContent: "space-evenly",
         width: "100%",
         minHeight: 56,
         py: 0,
@@ -41,38 +53,29 @@ const OnFieldBatsmenStats = ({ isSmall, data, isLoading, isAdmin = false }) => {
       ) : (
         <>
           {data && (
-            <Grid
-              container
-              sx={{
-                width: "100%",
-                display: "inline-flex",
-                alignItems: "center",
-                justifyContent: "space-between",
-              }}>
-              <Grid
-                sx={{
-                  display: "inline-flex",
-                  alignItems: "center",
-                  gap: 2,
-                  mr: 2,
-                }}>
-                {noOnStrikeBatsman ? (
-                  isAdmin ? (
-                    <SelectPlayer
-                      playerType={"On-Strike Batsman"}
-                      players={battingTeam.players}
-                      dispatchTarget={setOnStrikeBatsman}
-                      disabled={isLoading}
-                    />
-                  ) : (
-                    <SimpleTextFallback
-                      content={"On-Strike batsman data will appear here."}
-                    />
-                  )
+            <>
+              {noOnStrikeBatsman ? (
+                isAdmin ? (
+                  <SelectPlayer
+                    playerType={"On-Strike Batsman"}
+                    players={battingTeam.players}
+                    dispatchTarget={setOnStrikeBatsman}
+                    disabled={isLoading}
+                  />
                 ) : (
-                  <>
+                  <SimpleTextFallback
+                    content={"On-Strike batsman data will appear here."}
+                  />
+                )
+              ) : (
+                <Stack
+                  direction={"row"}
+                  width={"60%"}
+                  alignItems={"center"}
+                  justifyContent={"space-between"}>
+                  <Stack direction={"row"} spacing={1} alignItems={"center"}>
                     <img
-                      width={21}
+                      width={20}
                       src={`/assets/batsman_icon_${
                         isDarkTheme ? "light" : "dark"
                       }.svg`}
@@ -81,11 +84,17 @@ const OnFieldBatsmenStats = ({ isSmall, data, isLoading, isAdmin = false }) => {
                       {onStrikeBatsman.name} - {onStrikeBatsman.runs || 0} (
                       {onStrikeBatsman.ballsPlayed || 0})
                     </Typography>
-                  </>
-                )}
-              </Grid>
+                  </Stack>
+                  <IconButton
+                    color="neutral"
+                    size="sm"
+                    onClick={() => handlePlayerChange()}>
+                    <HiSelector />
+                  </IconButton>
+                </Stack>
+              )}
               <Divider orientation="vertical" sx={{ marginRight: 2 }} />
-              <Grid>
+              <>
                 {noOffStrikeBatsman ? (
                   isAdmin ? (
                     <SelectPlayer
@@ -100,13 +109,27 @@ const OnFieldBatsmenStats = ({ isSmall, data, isLoading, isAdmin = false }) => {
                     />
                   )
                 ) : (
-                  <Typography level={isSmall ? "body-xs" : "body-sm"}>
-                    {offStrikeBatsman.name} - {offStrikeBatsman.runs || 0} (
-                    {offStrikeBatsman.ballsPlayed || 0})
-                  </Typography>
+                  <Stack
+                    direction={"row"}
+                    spacing={1}
+                    width={"40%"}
+                    alignItems={"center"}
+                    justifyContent={"space-between"}>
+                    <Typography level={isSmall ? "body-xs" : "body-sm"}>
+                      {offStrikeBatsman.name} - {offStrikeBatsman.runs || 0} (
+                      {offStrikeBatsman.ballsPlayed || 0})
+                    </Typography>
+                    <IconButton
+                      color="neutral"
+                      size="sm"
+                      sx={{ marginLeft: 5 }}
+                      onClick={() => handlePlayerChange()}>
+                      <HiSelector />
+                    </IconButton>
+                  </Stack>
                 )}
-              </Grid>
-            </Grid>
+              </>
+            </>
           )}
         </>
       )}
