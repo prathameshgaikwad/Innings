d9cde3529a2912e8fb1ee47eaf87ecc09adcab9e
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-03 22:31:03 +0530

    fix: batting & bowling teams set wrong

--- server/helpers/setBattingAndBowlingTeamData.js
+++ server/helpers/setBattingAndBowlingTeamData.js
@@ -12,7 +12,7 @@ const setBattingAndBowlingTeamData = async ({
   const richTeam1Data = await getRichTeamData({ team_id: team1._id });
   const richTeam2Data = await getRichTeamData({ team_id: team2._id });
 
-  const isOddInnings = (innings.length + 1) % 2 === 1;
+  const isOddInnings = innings.length % 2 === 1;
   const isChoiceToBat = toss.decision === "bat";
   const tossWinningTeam = toss.winner_id.equals(team1._id)
     ? richTeam1Data
