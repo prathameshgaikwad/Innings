f3b18a2148d66bfa91a60bcc65db1904a1a76ed2
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-04 23:08:49 +0530

    refactor: improve prop structure

--- client/src/components/dataDisplay/DataChip.jsx
+++ client/src/components/dataDisplay/DataChip.jsx
@@ -2,17 +2,17 @@
 
 import { Box, Card, Divider, Grid, Typography } from "@mui/joy";
 
-const DataChip = ({ data }) => {
+const DataChip = ({ title, value }) => {
   return (
     <Grid xs={4}>
       <Card variant="outlined" sx={{ borderRadius: 5, p: 0 }}>
         <Box sx={{ display: "flex", justifyContent: "space-evenly" }}>
-          <Typography level="body-xs" sx={{ p: 0.5 }}>
-            {data[0]}
+          <Typography level="body-xs" sx={{ p: 0.5, px: 1 }}>
+            {title}
           </Typography>
           <Divider orientation="vertical" />
-          <Typography level="body-xs" sx={{ p: 0.5 }}>
-            {data[1]}
+          <Typography level="body-xs" sx={{ p: 0.5, px: 1 }}>
+            {value}
           </Typography>
         </Box>
       </Card>
