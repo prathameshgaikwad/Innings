2b778c65954e1422412e974aa89da644d91ba2e6
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-10 14:07:10 +0530

    fix: use .equals() comparator instead of ===

--- server/helpers/generateRichTossData.js
+++ server/helpers/generateRichTossData.js
@@ -2,7 +2,7 @@ const Team = require("../models/team");
 
 const generateRichTossData = async ({ toss, team1_id, team2_id }) => {
   const { name_short: winner_name } = await Team.findById(toss.winner_id);
-  const losingTeamId = toss.winner_id === team1_id ? team2_id : team1_id;
+  const losingTeamId = toss.winner_id.equals(team1_id) ? team2_id : team1_id;
   const { name_short: loser_name } = await Team.findById(losingTeamId);
 
   const richTossData = {
