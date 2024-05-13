34b6794ecd58752cf95156fa20215792d3dd74ce
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-13 22:56:51 +0530

    fix: player statistics not being updated

--- server/socket/handlers/addPlayerRuns.js
+++ server/socket/handlers/addPlayerRuns.js
@@ -8,15 +8,18 @@ const addPlayerRuns = ({ player_id, runs_scored }) => {
       player.statistics.total_runs += runs_scored;
       player.statistics.balls_faced += 1;
       player.statistics.strike_rate =
-        (total_runs / player.statistics.balls_faced) * 100;
-      player.statistics.average =
-        total_runs / player.statistics.total_dismissals;
+        (player.statistics.total_runs / player.statistics.balls_faced) * 100;
 
-      if (runs_scored === 4) {
-        player.statistics.fours++;
-      } else if (runs_scored === 6) {
-        player.statistics.sixes++;
+      if (player.statistics.total_dismissals > 0) {
+        player.statistics.average =
+          player.statistics.total_runs / player.statistics.total_dismissals;
+      } else {
+        player.statistics.average = 0;
       }
+
+      if (runs_scored === 4) player.statistics.fours++;
+      else if (runs_scored === 6) player.statistics.sixes++;
+
       await player.save();
     };
     updatePlayerRunLog();
