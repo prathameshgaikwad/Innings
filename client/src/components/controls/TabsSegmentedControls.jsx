a48461df66555e0f7ca622a95e30de92af6908c2
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-16 21:27:41 +0530

    style: add enter delay of 1500 ms to tooltip

--- client/src/components/controls/TabsSegmentedControls.jsx
+++ client/src/components/controls/TabsSegmentedControls.jsx
@@ -29,6 +29,7 @@ const TabsSegmentedControls = ({
           [`& .${tabClasses.root}[aria-selected="true"]`]: {
             color: theme.palette.neutral.plainColor,
             bgcolor: theme.palette.primary.plainActiveBg,
+            transition: "all 0.25s ease-in-out",
             "&:hover": {
               bgcolor: theme.palette.primary.plainHoverBg,
             },
@@ -38,6 +39,7 @@ const TabsSegmentedControls = ({
           title={battingTeam.name}
           placement="top"
           variant="outlined"
+          enterDelay={1500}
           arrow>
           <Tab disableIndicator>{battingTeam.name_short} Innings</Tab>
         </Tooltip>
