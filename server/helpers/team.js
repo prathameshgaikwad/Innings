cdadfe2d256bc6682766b7bb749a8e417b4ab7e0
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-16 16:57:00 +0530

    refactor: shift helpers to team.js under helpers

--- server/helpers/team.js
+++ server/helpers/team.js
@@ -1,4 +1,42 @@
-const { getRichTeamData } = require("./getRichTeamData");
+const Player = require("../models/player");
+const Team = require("../models/team");
+
+const getRichTeamData = async ({ team_id }) => {
+  try {
+    const teamData = await Team.findById(team_id);
+    const { players } = teamData;
+
+    const captain_id = players.find((player) => player.is_captain);
+    const captain = await Player.findById(captain_id);
+    const captain_name = `${captain.first_name} ${captain.last_name}`;
+
+    const richPlayersData = await Promise.all(
+      players.map(async (player) => {
+        const playerData = await Player.findById(player._id);
+        const richPlayerData = playerData.toObject();
+        return richPlayerData;
+      })
+    );
+
+    richPlayersData.sort((a, b) => {
+      const playerA = `${a.first_name} ${a.last_name}`.toLowerCase();
+      const playerB = `${b.first_name} ${b.last_name}`.toLowerCase();
+      if (playerA < playerB) return -1;
+      if (playerA > playerB) return 1;
+      return 0;
+    });
+
+    const richTeamData = {
+      ...teamData.toObject(),
+      players: richPlayersData,
+      captain_name,
+    };
+
+    return richTeamData;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 const setBattingAndBowlingTeamData = async ({
   innings,
@@ -57,4 +95,4 @@ const setBattingAndBowlingTeamData = async ({
   return { battingTeam, bowlingTeam };
 };
 
-module.exports = { setBattingAndBowlingTeamData };
+module.exports = { setBattingAndBowlingTeamData, getRichTeamData };
