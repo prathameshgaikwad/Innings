ba64bcc710f040d075ea332ebaf22a3615de6497
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-19 20:03:13 +0530

    refactor: separate JoinTournamentButton into component

---
+++ client/src/components/buttons/JoinTournamentButton.jsx
@@ -0,0 +1,100 @@
+/* eslint-disable react/prop-types */
+
+import { useDispatch, useSelector } from "react-redux";
+
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import { Button } from "@mui/joy";
+import { addJoinedTournament } from "../../state/tournament/tournamentSlice";
+import { tournamentPageApi } from "../../services/api";
+
+const handleJoinTournament = async ({
+  dispatch,
+  user_id,
+  token,
+  tournament_id,
+  setIsOpen,
+}) => {
+  try {
+    dispatch(
+      tournamentPageApi.joinTournament({ user_id, token, tournament_id })
+    );
+    dispatch(addJoinedTournament({ tournament_id }));
+    setIsOpen(true);
+
+    setTimeout(() => {
+      setIsOpen(false);
+    }, 3000);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const JoinTournamentButton = ({
+  user_id,
+  token,
+  tournament_id,
+  setIsOpen,
+  isLoading,
+}) => {
+  const dispatch = useDispatch();
+  const { joinedTournaments, createdTournaments } = useSelector(
+    (state) => state.tournaments
+  );
+
+  let buttonDisplay = !isLoading ? "flex" : "none";
+
+  if (joinedTournaments.includes(tournament_id)) buttonDisplay = "none";
+  if (createdTournaments.includes(tournament_id)) buttonDisplay = "none";
+
+  return (
+    <Button
+      sx={{
+        height: 90,
+        width: 90,
+        borderRadius: 50,
+        position: "absolute",
+        bottom: -45,
+        right: 100,
+        overflow: "hidden",
+        display: buttonDisplay,
+        border: "2px solid",
+        transition: "all 0.2s ease-in-out",
+        zIndex: 20,
+        "&:hover": {
+          borderWidth: 4,
+          backgroundColor: "var(--joy-palette-primary-solidBg)",
+          "&:after ": {
+            left: "130%",
+            transition: " all 550ms cubic-bezier(0.19, 1, 0.22, 1)",
+          },
+        },
+        "&:after": {
+          background: "#fff",
+          content: '""',
+          height: "155px",
+          left: "-65px",
+          opacity: ".2",
+          position: "absolute",
+          top: "-40px",
+          transform: "rotate(35deg)",
+          transition: "all 550ms cubic-bezier(0.19, 1, 0.22, 1)",
+          width: "50px",
+          zIndex: -10,
+        },
+      }}
+      endDecorator={<ArrowForwardIcon />}
+      onClick={() =>
+        handleJoinTournament({
+          dispatch,
+          user_id,
+          tournament_id,
+          token,
+          setIsOpen,
+        })
+      }>
+      Join
+    </Button>
+  );
+};
+
+export default JoinTournamentButton;
--- client/src/components/tournament/TournamentBanner.jsx
+++ client/src/components/tournament/TournamentBanner.jsx
@@ -2,7 +2,6 @@
 
 import {
   AspectRatio,
-  Button,
   Card,
   CardCover,
   CardOverflow,
@@ -11,15 +10,13 @@ import {
   Typography,
   useTheme,
 } from "@mui/joy";
-import { useDispatch, useSelector } from "react-redux";
 
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import CustomToast from "../notifications/toasts/CustomToast";
+import JoinTournamentButton from "../buttons/JoinTournamentButton";
 import { MdOutlineAdminPanelSettings } from "react-icons/md";
 import { MdOutlineBuildCircle } from "react-icons/md";
-import { addJoinedTournament } from "../../state/tournament/tournamentSlice";
-import { tournamentPageApi } from "../../services/api";
 import { useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
 import { useState } from "react";
 
 const TournamentBanner = ({
@@ -31,41 +28,11 @@ const TournamentBanner = ({
   isLoading,
 }) => {
   const theme = useTheme();
-  const dispatch = useDispatch();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   const user_id = useSelector((state) => state.user.user._id);
   const token = useSelector((state) => state.user.token);
 
-  const joinedTournaments = useSelector(
-    (state) => state.tournaments.joinedTournaments
-  );
-
-  const createdTournaments = useSelector(
-    (state) => state.tournaments.createdTournaments
-  );
-
-  let buttonDisplay = !isLoading ? "flex" : "none";
-
-  if (joinedTournaments.includes(id)) buttonDisplay = "none";
-  if (createdTournaments.includes(id)) buttonDisplay = "none";
-
   const [isOpen, setIsOpen] = useState(false);
-
-  const handleJoinTournament = async () => {
-    try {
-      dispatch(
-        tournamentPageApi.joinTournament({ user_id, token, tournament_id: id })
-      );
-      dispatch(addJoinedTournament({ tournament_id: id }));
-      setIsOpen(true);
-
-      setTimeout(() => {
-        setIsOpen(false);
-      }, 3000);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   return (
     <>
@@ -142,45 +109,13 @@ const TournamentBanner = ({
               </Typography>
             </Link>
           </CardCover>
-          <Button
-            sx={{
-              height: 90,
-              width: 90,
-              borderRadius: 50,
-              position: "absolute",
-              bottom: -45,
-              right: 100,
-              overflow: "hidden",
-              display: buttonDisplay,
-              border: "2px solid",
-              transition: "all 0.2s ease-in-out",
-              zIndex: 20,
-              "&:hover": {
-                borderWidth: 4,
-                backgroundColor: "var(--joy-palette-primary-solidBg)",
-                "&:after ": {
-                  left: "130%",
-                  transition: " all 550ms cubic-bezier(0.19, 1, 0.22, 1)",
-                },
-              },
-              "&:after": {
-                background: "#fff",
-                content: '""',
-                height: "155px",
-                left: "-65px",
-                opacity: ".2",
-                position: "absolute",
-                top: "-40px",
-                transform: "rotate(35deg)",
-                transition: "all 550ms cubic-bezier(0.19, 1, 0.22, 1)",
-                width: "50px",
-                zIndex: -10,
-              },
-            }}
-            endDecorator={<ArrowForwardIcon />}
-            onClick={() => handleJoinTournament()}>
-            Join
-          </Button>
+          <JoinTournamentButton
+            user_id={user_id}
+            tournament_id={id}
+            token={token}
+            setIsOpen={setIsOpen}
+            isLoading={isLoading}
+          />
         </CardOverflow>
       </Card>
     </>
