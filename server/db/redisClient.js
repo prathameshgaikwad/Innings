7cc4c755e0b1b69f38c97f64a32a1bddc5a67764
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-24 14:40:16 +0530

    refactor: use redis port environment variable

--- server/db/redisClient.js
+++ server/db/redisClient.js
@@ -1,6 +1,7 @@
 const { createClient } = require("redis");
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
-const redisClient = createClient();
+const redisClient = createClient(REDIS_PORT);
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
