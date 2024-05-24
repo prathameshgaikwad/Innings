876de270503d55804861db377f2d7b153e72da58
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-24 14:44:57 +0530

    fix: pass redis port using options object

--- server/db/redisClient.js
+++ server/db/redisClient.js
@@ -1,7 +1,7 @@
 const { createClient } = require("redis");
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 
-const redisClient = createClient(REDIS_PORT);
+const redisClient = createClient({ port: REDIS_PORT });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 
