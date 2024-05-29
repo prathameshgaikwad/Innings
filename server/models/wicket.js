0a88d3ac94a3903de415f56980c43f081a059d33
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-29 22:40:36 +0530

    refactor: remove unused wicketSchema

--- server/models/wicket.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const { wicketSchema } = require("./schemas/wicketSchema");
-
-const Wicket = mongoose.model("wicket", wicketSchema);
-
-module.exports = Wicket;
