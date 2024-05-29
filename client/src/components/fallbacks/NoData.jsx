28ebdaad601151febd4bc24ba9e2f3d483eb7945
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-29 22:26:13 +0530

    style: make component styles more subtle

--- client/src/components/fallbacks/NoData.jsx
+++ client/src/components/fallbacks/NoData.jsx
@@ -13,15 +13,19 @@ const NoData = ({ height, isSmall, customStyles }) => {
     justifyContent: "center",
     height: height || 300,
     width: "100%",
+    opacity: 0.6,
   };
 
   return (
     <Card sx={{ ...defaultStyles, ...customStyles }}>
       <RiEmotionSadLine
         fontSize={isSmall ? 64 : 92}
-        style={{ color: theme.palette.neutral[800] }}
+        style={{ color: theme.palette.neutral.softDisabledBg }}
       />
-      <Typography color="neutral" level={isSmall ? "body-xs" : "body-md"}>
+      <Typography
+        color="neutral"
+        level={isSmall ? "body-xs" : "body-md"}
+        textColor={"text.tertiary"}>
         No data to display
       </Typography>
     </Card>
