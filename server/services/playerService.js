b7e30a95ff158c0c7cdc3f33b7f20476cf626f52
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-05-31 15:19:24 +0530

    refactor: function to update player bowling statistics

--- server/services/playerService.js
+++ server/services/playerService.js
@@ -13,4 +13,35 @@ async function updatePlayerBattingStatistics({ player_id, runs_scored }) {
   );
 }
 
-module.exports = { updatePlayerBattingStatistics };
+async function updatePlayerBowlingStatistics({
+  player_id,
+  runs_conceded,
+  valid_ball,
+  is_wicket,
+  is_extra,
+  extra_type,
+}) {
+  await Player.updateOne(
+    { _id: player_id },
+    {
+      $inc: {
+        "statistics.balls_bowled": valid_ball ? 1 : 0,
+        "statistics.runs_conceded": runs_conceded,
+        "statistics.wickets_taken": is_wicket ? 1 : 0,
+        "statistics.extras.wides": is_extra && extra_type.equals("WD") ? 1 : 0,
+        "statistics.extras.no_balls":
+          is_extra && extra_type.equals("NB") ? 1 : 0,
+        "statistics.extras.byes": is_extra && extra_type.equals("B") ? 1 : 0,
+        "statistics.extras.leg_byes":
+          is_extra && extra_type.equals("LB") ? 1 : 0,
+        "statistics.extras.penalty_runs":
+          is_extra && extra_type.equals("P") ? 1 : 0,
+      },
+    }
+  );
+}
+
+module.exports = {
+  updatePlayerBattingStatistics,
+  updatePlayerBowlingStatistics,
+};
