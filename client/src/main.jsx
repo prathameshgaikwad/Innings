30acb4595607b3a534b6778e7b127d105daa750d
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-06-14 18:09:24 +0530

    fix: remove tanstack/react-query

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { persistor, store } from "./store.js";
 
 import App from "./App.jsx";
@@ -11,18 +10,14 @@ import ReactDOM from "react-dom/client";
 const GOOGLE_OAUTH_CLIENT_ID = import.meta.env
   .VITE_SERVER_GOOGLE_OAUTH_CLIENT_ID;
 
-const queryClient = new QueryClient();
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
-        <Provider store={store}>
-          <PersistGate loading={null} persistor={persistor}>
-            <App />
-          </PersistGate>
-        </Provider>
-      </GoogleOAuthProvider>
-    </QueryClientProvider>
+    <GoogleOAuthProvider clientId={GOOGLE_OAUTH_CLIENT_ID}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
