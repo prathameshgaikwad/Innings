fba0421f9188e97fd4eaf32091ca9e69f51f57bf
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-06-16 16:47:22 +0530

    refactor: log redis client connected when client connects

--- server/db/redisClient.js
+++ server/db/redisClient.js
@@ -4,6 +4,9 @@ const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const redisClient = createClient({ port: REDIS_PORT });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("connect", () =>
+  console.log("Redis Client Connected! Caching is active :)")
+);
 
 const connectRedis = async () => {
   await redisClient.connect();
