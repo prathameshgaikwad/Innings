622d557c2455efba5a17c5398ac70381e8b1a539
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-06-17 19:19:19 +0530

    perf: use react lazy & suspense

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,29 @@
 /* eslint-disable react/prop-types */
 
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Suspense, lazy, useEffect } from "react";
 
-import CreateAccount from "./pages/accounts/CreateAccount";
 import CssBaseline from "@mui/joy/CssBaseline";
 import { CssVarsProvider } from "@mui/joy";
-import HomePage from "./pages/home/Home";
-import Match from "./pages/match/Match";
-import MatchManagement from "./pages/match/MatchManagement";
-import MyStatistics from "./pages/statistics/MyStatistics";
-import SignIn from "./pages/accounts/SignIn";
-import Statistics from "./pages/statistics/Statistics";
-import TeamPage from "./pages/team/TeamPage";
-import TournamentManagement from "./pages/tournaments/TournamentManagement";
-import TournamentPage from "./pages/tournaments/TournamentPage";
-import TournamentSetupPage from "./pages/tournaments/TournamentSetupPage";
-import Tournaments from "./pages/tournaments/Tournaments";
 import myTheme from "./theme";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
+
+const CreateAccount = lazy(() => import("./pages/accounts/CreateAccount"));
+const HomePage = lazy(() => import("./pages/home/Home"));
+const Match = lazy(() => import("./pages/match/Match"));
+const MatchManagement = lazy(() => import("./pages/match/MatchManagement"));
+const MyStatistics = lazy(() => import("./pages/statistics/MyStatistics"));
+const SignIn = lazy(() => import("./pages/accounts/SignIn"));
+const Statistics = lazy(() => import("./pages/statistics/Statistics"));
+const TeamPage = lazy(() => import("./pages/team/TeamPage"));
+const TournamentManagement = lazy(() =>
+  import("./pages/tournaments/TournamentManagement")
+);
+const TournamentPage = lazy(() => import("./pages/tournaments/TournamentPage"));
+const TournamentSetupPage = lazy(() =>
+  import("./pages/tournaments/TournamentSetupPage")
+);
+const Tournaments = lazy(() => import("./pages/tournaments/Tournaments"));
 
 const App = () => {
   const PrivateRoute = ({ children }) => {
@@ -38,93 +43,95 @@ const App = () => {
         disableTransitionOnChange
         theme={myTheme}>
         <CssBaseline />
-        <Routes>
-          {/* PUBLIC PATHS */}
-          <Route path="/accounts/sign-in" element={<SignIn />} />
-          <Route path="/accounts/create" element={<CreateAccount />} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            {/* PUBLIC PATHS */}
+            <Route path="/accounts/sign-in" element={<SignIn />} />
+            <Route path="/accounts/create" element={<CreateAccount />} />
 
-          {/* PRIVATE PATHS */}
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <HomePage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tournaments"
-            element={
-              <PrivateRoute>
-                <Tournaments />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tournaments/:tournamentId"
-            element={
-              <PrivateRoute>
-                <TournamentPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tournaments/:tournamentId/setup"
-            element={
-              <PrivateRoute>
-                <TournamentSetupPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tournaments/:tournamentId/manage"
-            element={
-              <PrivateRoute>
-                <TournamentManagement />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tournaments/:tournamentId/match/:matchId"
-            element={
-              <PrivateRoute>
-                <Match />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tournaments/:tournamentId/match/:matchId/manage"
-            element={
-              <PrivateRoute>
-                <MatchManagement />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/tournaments/:tournamentId/teams/:teamId"
-            element={
-              <PrivateRoute>
-                <TeamPage />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/statistics"
-            element={
-              <PrivateRoute>
-                <Statistics />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/user/statistics"
-            element={
-              <PrivateRoute>
-                <MyStatistics />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
+            {/* PRIVATE PATHS */}
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <HomePage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tournaments"
+              element={
+                <PrivateRoute>
+                  <Tournaments />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tournaments/:tournamentId"
+              element={
+                <PrivateRoute>
+                  <TournamentPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tournaments/:tournamentId/setup"
+              element={
+                <PrivateRoute>
+                  <TournamentSetupPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tournaments/:tournamentId/manage"
+              element={
+                <PrivateRoute>
+                  <TournamentManagement />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tournaments/:tournamentId/match/:matchId"
+              element={
+                <PrivateRoute>
+                  <Match />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tournaments/:tournamentId/match/:matchId/manage"
+              element={
+                <PrivateRoute>
+                  <MatchManagement />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tournaments/:tournamentId/teams/:teamId"
+              element={
+                <PrivateRoute>
+                  <TeamPage />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/statistics"
+              element={
+                <PrivateRoute>
+                  <Statistics />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/user/statistics"
+              element={
+                <PrivateRoute>
+                  <MyStatistics />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
       </CssVarsProvider>
     </BrowserRouter>
   );
