4b7ba6b547ff2c04b61713fb28729e3bf388e0b4
BHUSHAN <bhushannalawade318@gmail.com>
2024-08-05 23:17:56 +0530

    fix #15

--- client/src/components/formComponents/CustomInput.tsx
+++ client/src/components/formComponents/CustomInput.tsx
@@ -3,19 +3,26 @@ import {
   FormHelperText,
   FormLabel,
   Input,
+  IconButton,
   InputProps,
   Typography,
 } from "@mui/joy";
-
+import React, { useState } from "react";
 import { useField } from "formik";
-
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 interface CustomInputProps extends Omit<InputProps, "name"> {
   label: string;
   name: string;
 }
 
-const CustomInput: React.FC<CustomInputProps> = ({ label, ...props }) => {
+const CustomInput: React.FC<CustomInputProps> = ({ label,type, ...props }) => {
   const [field, meta] = useField(props.name);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
 
   return (
     <FormControl>
@@ -23,8 +30,18 @@ const CustomInput: React.FC<CustomInputProps> = ({ label, ...props }) => {
       <Input
         {...field}
         {...props}
+        type={type === "password" && showPassword ? "text" : type}
         autoComplete="off"
         value={field.value || ""}
+        endDecorator={
+          type === "password" ? (
+            <IconButton
+              onClick={togglePasswordVisibility}
+              // sx={{ position: "absolute", right: "8px", backgroundColor:"gray", width:"10px", height:"5px"}}
+            >{showPassword ? <VisibilityOff /> : <Visibility />}
+            </IconButton>
+          ) : null
+        }
         sx={{ fontSize: "sm" }}
       />
       {meta.touched && meta.error && (
