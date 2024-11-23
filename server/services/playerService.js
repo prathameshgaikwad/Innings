2ab154748b05cebf740a6fd113233c7a82378b22
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2024-11-23 12:34:11 +0530

    refactor: separate out constants

--- server/services/playerService.js
+++ server/services/playerService.js
@@ -1,4 +1,5 @@
 const Player = require("../models/player");
+const { EXTRA_TYPE } = require("../utils/constants");
 async function updatePlayerBattingStatistics({ player_id, runs_scored }) {
   await Player.updateOne(
     { _id: player_id },
@@ -28,14 +29,16 @@ async function updatePlayerBowlingStatistics({
         "statistics.balls_bowled": valid_ball ? 1 : 0,
         "statistics.runs_conceded": runs_conceded,
         "statistics.wickets_taken": is_wicket ? 1 : 0,
-        "statistics.extras.wides": is_extra && extra_type.equals("WD") ? 1 : 0,
+        "statistics.extras.wides":
+          is_extra && extra_type.equals(EXTRA_TYPE.WIDE) ? 1 : 0,
         "statistics.extras.no_balls":
-          is_extra && extra_type.equals("NB") ? 1 : 0,
-        "statistics.extras.byes": is_extra && extra_type.equals("B") ? 1 : 0,
+          is_extra && extra_type.equals(EXTRA_TYPE.NO_BALL) ? 1 : 0,
+        "statistics.extras.byes":
+          is_extra && extra_type.equals(EXTRA_TYPE.BYE) ? 1 : 0,
         "statistics.extras.leg_byes":
-          is_extra && extra_type.equals("LB") ? 1 : 0,
+          is_extra && extra_type.equals(EXTRA_TYPE.LEG_BYE) ? 1 : 0,
         "statistics.extras.penalty_runs":
-          is_extra && extra_type.equals("P") ? 1 : 0,
+          is_extra && extra_type.equals(EXTRA_TYPE.PENALTY) ? 1 : 0,
       },
     }
   );
---
+++ server/utils/constants.ts
@@ -0,0 +1,7 @@
+export const EXTRA_TYPE = {
+  WIDE: "WD",
+  NO_BALL: "NB",
+  BYE: "B",
+  LEG_BYE: "LB",
+  PENALTY: "P",
+} as const;
