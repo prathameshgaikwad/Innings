342fab2c0cadf9add870c1ebaa67010ff776cf8b
Prathamesh Gaikwad <37247788+prathameshgaikwad@users.noreply.github.com>
2026-02-24 19:44:43 +0530

    infra: use redis host from env

--- server/db/redisClient.js
+++ server/db/redisClient.js
@@ -1,7 +1,13 @@
 const { createClient } = require("redis");
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_HOST = process.env.REDIS_HOST || "127.0.0.1";
 
-const redisClient = createClient({ port: REDIS_PORT });
+const redisClient = createClient({
+  socket: {
+    host: REDIS_HOST,
+    port: REDIS_PORT
+  }
+});
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
 redisClient.on("connect", () =>
